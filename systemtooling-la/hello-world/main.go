afa89432b8f916dfdba1c1065f14e3cad37b9718
Ramon Carreras <rcarreras.01@gmail.com>
2020-09-12 11:42:51 -0400

    First commit of code written during the System Tooling in Go course on Linux Academy

---
+++ systemtooling-la/hello-world/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+/*
+this is a comment
+on multiple lines
+*/
+
+// main is the primary function
+// This function ...
+func main() {
+	fmt.Println("Hello Linux Academy") // trailing comment
+}
